ef8ef670a68b7a985e90ae69945514c67b815771
Joshua Leuenberger <18721625+yehlo@users.noreply.github.com>
2026-01-26 18:12:18 +0100

    Feat/s3bu name (#70)
    * feat: introduce spec.bucketName to let user choose bucketName
    
    * fix(bucket): allow update of spec.bucketName if the bucket was not created yet

--- internal/webhook/s3bucket_webhook.go
+++ internal/webhook/s3bucket_webhook.go
@@ -118,8 +118,11 @@ func (r *S3BucketValidator) ValidateUpdate(ctx context.Context, oldObj, newObj r
 	}
 
 	// although the controller will just ignore updates to spec.bucketName anyway, we want to add some verbosity for the users.
-	if s3bucketOld.Spec.BucketName != nil && s3bucketNew.Spec.BucketName != nil && *s3bucketOld.Spec.BucketName != *s3bucketNew.Spec.BucketName {
-		return nil, fmt.Errorf("spec.bucketName can only be set during creation")
+	// Allowing changes only if the bucket is still in pending phase (i.e. not yet created).
+	if s3bucketOld.Status.Phase != s3v1alpha1.BucketPhasePending {
+		if s3bucketOld.Spec.BucketName != nil && s3bucketNew.Spec.BucketName != nil && *s3bucketOld.Spec.BucketName != *s3bucketNew.Spec.BucketName {
+			return nil, fmt.Errorf("spec.bucketName can only be set during creation")
+		}
 	}
 
 	return nil, nil
